e3c11cd87edd6eb2d58b4d5d683dcdff86a70e6f
FlyDragonAtShy <1539572245@qq.com>
2019-05-16 17:15:23 +0800

    random
    random

---
+++ random.php
@@ -0,0 +1,18 @@
+$img_src = "https://i.loli.net/2019/05/16/5cdd274cada1650609.jpg|https://i.loli.net/2019/05/16/5cdd274d1ba4a60932.jpg|https://i.loli.net/2019/05/16/5cdd274d3f65212990.jpg|https://i.loli.net/2019/05/16/5cdd274d47bf684082.jpg|https://i.loli.net/2019/05/16/5cdd274d4f86527475.jpg|https://i.loli.net/2019/05/16/5cdd274d52fa363939.jpg|https://i.loli.net/2019/05/16/5cdd274d5610284228.jpg|https://i.loli.net/2019/05/16/5cdd274d583f051088.jpg|https://i.loli.net/2019/05/16/5cdd274d5a18f77957.jpg|https://i.loli.net/2019/05/16/5cdd274d5c12340883.jpg";
+
+$img_array = explode("|", $img_src);
+
+$img = array_rand($img_array); 
+
+$imgurl=$img_array[$img]; 
+
+https://sm.ms/delete/norawDpuCVhc23K
+https://sm.ms/delete/fhoOEJg35kSU1lI
+https://sm.ms/delete/5jqWG6CpnZV79lm
+https://sm.ms/delete/canXY597okhgBvU
+https://sm.ms/delete/tiEgfsRThYJXFyM
+https://sm.ms/delete/jmhvtBksSp2aL8P
+https://sm.ms/delete/JceAKphWaBLZyRi
+https://sm.ms/delete/SsuqKP7OvXmrECL
+https://sm.ms/delete/BAKptwlbfFSjU3Y
+https://sm.ms/delete/Tst8SrmZwAhXo2f
